d675723a2abd3524f2e74e52404d106f7aa6c548
heqin <heqin@asiainfo.com>
2021-05-09 02:22:52 +0800

    spring配置文件中<bean>的id和name属性区别

--- spring-01-ioc/src/main/java/com/linkedbear/spring/annotation/d_importxml/ImportXmlAnnoApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/annotation/d_importxml/ImportXmlAnnoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class ImportXmlAnnoApplication {
@@ -17,5 +18,16 @@ public class ImportXmlAnnoApplication {
         
         ApplicationContext ctx2 = new AnnotationConfigApplicationContext(ImportXmlAnnotationConfiguration.class);
         Stream.of(ctx2.getBeanDefinitionNames()).forEach(System.out::println);
+
+        //spring配置文件中<bean>的id和name属性区别，以及identifier和aliases
+        //https://blog.csdn.net/aitangyong/article/details/50629525
+        System.out.println("---------测试别名---------");
+        String[] aliases = ctx2.getAliases("person");
+        System.out.println(Arrays.toString(aliases));
+
+
+        String[] alias = ctx2.getAliases("boyMan");
+        System.out.println(Arrays.toString(alias));//[a1, a2]
+
     }
 }
