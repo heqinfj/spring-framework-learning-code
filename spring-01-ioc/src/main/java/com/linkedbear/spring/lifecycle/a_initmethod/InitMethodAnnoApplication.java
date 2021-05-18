46889ed7ac9d76cdd1b5b29f1d75cdbb262c709c
heqin <heqin@asiainfo.com>
2021-05-18 11:27:23 +0800

    Bean 的生命周期中，是先对属性赋值，后执行 init-method 标记的方法

--- spring-01-ioc/src/main/java/com/linkedbear/spring/lifecycle/a_initmethod/InitMethodAnnoApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/lifecycle/a_initmethod/InitMethodAnnoApplication.java
@@ -12,3 +12,13 @@ public class InitMethodAnnoApplication {
         ctx.close();
     }
 }
+/**
+ 输出结果如下：
+ Dog 构造方法执行了。。。
+ setName方法执行了。。。
+ wangwang被初始化了。。。
+ IOC容器初始化完成。。。
+ wangwang被销毁了。。。
+
+ //由此可以得出结论：Bean 的生命周期中，是先对属性赋值，后执行 init-method 标记的方法。
+ */
