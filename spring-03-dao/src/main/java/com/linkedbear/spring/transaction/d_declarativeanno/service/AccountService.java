6dd999193b5d5d27ec0cda6651af813a764fc94c
heqinfj <heqinfj@163.com>
2021-07-26 10:22:51 +0800

    1.解决事务嵌套捕获异常时发生的问题：org.springframework.transaction.UnexpectedRollbackException: Transaction rolled back because it has been marked as rollback-only TransactionAspectSupport.currentTransactionStatus().setRollbackOnly() 2.获取方法所处的当前事务名称 TransactionSynchronizationManager.getCurrentTransactionName()

--- spring-03-dao/src/main/java/com/linkedbear/spring/transaction/d_declarativeanno/service/AccountService.java
+++ spring-03-dao/src/main/java/com/linkedbear/spring/transaction/d_declarativeanno/service/AccountService.java
@@ -4,6 +4,8 @@ import com.linkedbear.spring.transaction.d_declarativeanno.dao.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 @Transactional
@@ -11,12 +13,26 @@ public class AccountService {
 
     @Autowired
     AccountDao accountDao;
-    
+
+    @Autowired
+    UserService userService;
+
     public void transfer(Integer sourceId, Integer targetId, int money) {
+        StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[1];
+        //获取方法所处的当前事务名称  TransactionSynchronizationManager.getCurrentTransactionName()
+        System.out.println(String.format("当前类：%s，当前方法：%s，当前事务的名称：%s",stackTraceElement.getClassName(),stackTraceElement.getMethodName(), TransactionSynchronizationManager.getCurrentTransactionName()));
         accountDao.subtractMoney(sourceId, money);
-        
-        int i = 1 / 0;
-        
+
+        //int i = 1 / 0;
+
         accountDao.addMoney(targetId, money);
+        try {
+            userService.saveAndQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            //当前示例事务传播行为为REQUIRED，当userService.saveAndQuery()发生异常时，并用try catch捕获时，则会抛出如下异常信息：
+            //解决事务嵌套捕获异常时发生的问题：org.springframework.transaction.UnexpectedRollbackException: Transaction rolled back because it has been marked as rollback-only
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
     }
 }
--- spring-03-dao/src/main/java/com/linkedbear/spring/transaction/d_declarativeanno/service/UserService.java
+++ spring-03-dao/src/main/java/com/linkedbear/spring/transaction/d_declarativeanno/service/UserService.java
@@ -5,6 +5,7 @@ import com.linkedbear.spring.transaction.d_declarativeanno.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.List;
 
@@ -16,6 +17,9 @@ public class UserService {
     
     @Transactional
     public void saveAndQuery() {
+        //StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[1];
+        System.out.println(String.format("当前类：%s，当前方法：%s，当前事务的名称：%s",stackTraceElement.getClassName(),stackTraceElement.getMethodName(), TransactionSynchronizationManager.getCurrentTransactionName()));
         User user = new User();
         user.setName("阿巴阿巴");
         user.setTel("123654789");
